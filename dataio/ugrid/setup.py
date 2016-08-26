54e53f0f8397ff6b44398f33cc90d4b98df15dc5
Matt Rayson <Matt Rayson>
2016-08-26 13:31:23 +0800

    Modified setup.py tp work on windows-64

--- dataio/ugrid/setup.py
+++ dataio/ugrid/setup.py
@@ -1,15 +1,25 @@
 """
 Build with:
      python setup.py build_ext --inplace
+
+See this site for building on windows-64:
+	https://github.com/cython/cython/wiki/CythonExtensionsOnWindows
 """
 
-from distutils.core import setup
-from distutils.extension import Extension
+#from distutils.core import setup
+#from distutils.extension import Extension
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+
 from Cython.Build import cythonize
 import os
 import numpy
 
-os.environ["CC"]='cc'
+os.environ["CC"]='cl'
 
 extensions =[
     Extension("ugridutils",["ugridutils.pyx"],
