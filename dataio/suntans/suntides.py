Changed suntides to accept 3D arrays (faster). Bug fixes to dask matrix computation

dataio/suntans/suntides.py
@@ -31,6 +31,7 @@ class suntides(Spatial):
     
     frqnames = None
     baseyear = 1990 # All phases are referenced to the 1st of the 1st of this year
+    fit_all_k = False
     
     def __init__(self,ncfile,**kwargs):
         """
@@ -105,29 +106,48 @@ class suntides(Spatial):
                 self.Var[vv] = np.var(data, axis=0)
 
             elif ndim == 3:
-                for k in range(self.Nkmax):
-                    self.klayer=[k]
-                    print('Loading data...')
-                    data = self.loadData()
-                    print('Performing harmonic fit on variable, %s, layer = %d of %d...'%(self.variable,self.klayer[0],self.Nkmax))
-                    self.Amp[vv][:,k,:], self.Phs[vv][:,k,:], self.Mean[vv][k,:] =\
-                        harmonic_fit(time, data, self.frq, \
-                        phsbase=self.reftime,\
-                        #phsbase=None,
-                        )
-
-
-                    # Preserve the mask
-                    if isinstance(data, np.ma.MaskedArray):
-                        #mask = data[0,:] == self._FillValue
-                        mask = data.mask[0,:]
-                        self.Mean[vv][k,mask] = self._FillValue
-                        for ff in range(len(self.frq)):
-                            self.Amp[vv][ff,k,mask] = self._FillValue
-                            self.Phs[vv][ff,k,mask] = self._FillValue
-
+                if self.fit_all_k == False:
+                    self.harmonic_fit_k()
+                else:
+                    self.klayer=[-99]
+                    print('Loading 3D data from %s...'%vv)
+                    data=self.loadData()
+                    print('Performing harmonic fit on variable, %s... (all depths)'%(self.variable))
+                    self.Amp[vv], self.Phs[vv], self.Mean[vv] =\
+                            harmonic_fit(time, data, self.frq, \
+                            phsbase=self.reftime,\
+                            #phsbase=None,\
+                            )
                     # Compute the variance of the original signal
-                    self.Var[vv][k,:] = np.var(data, axis=0)
+                    self.Var[vv] = np.var(data, axis=0)
+
+                    
+
+
+    def harmonic_fit_k(self):
+        for k in range(self.Nkmax):
+            self.klayer=[k]
+            print('Loading data...')
+            data = self.loadData()
+            print('Performing harmonic fit on variable, %s, layer = %d of %d...'%(self.variable,self.klayer[0],self.Nkmax))
+            self.Amp[vv][:,k,:], self.Phs[vv][:,k,:], self.Mean[vv][k,:] =\
+                harmonic_fit(time, data, self.frq, \
+                phsbase=self.reftime,\
+                #phsbase=None,
+                )
+
+
+            # Preserve the mask
+            if isinstance(data, np.ma.MaskedArray):
+                #mask = data[0,:] == self._FillValue
+                mask = data.mask[0,:]
+                self.Mean[vv][k,mask] = self._FillValue
+                for ff in range(len(self.frq)):
+                    self.Amp[vv][ff,k,mask] = self._FillValue
+                    self.Phs[vv][ff,k,mask] = self._FillValue
+
+            # Compute the variance of the original signal
+            self.Var[vv][k,:] = np.var(data, axis=0)
 
                     
     def _prepDict(self,varnames):
@@ -706,11 +726,11 @@ if __name__ == '__main__':
             varnames = val.split()
     
     try:
-	ncfile = sys.argv[1]
-	outfile = sys.argv[2]
+        ncfile = sys.argv[1]
+        outfile = sys.argv[2]
     except:
-    	usage()
-	exit(1)
+        usage()
+        exit(1)
     
     print(ncfile, outfile, varnames, frqnames)
     # Call the object

dataio/suntans/sunxray.py
@@ -4,11 +4,13 @@ classes and xray/dask.
 """
 
 import xarray as xr
+from netCDF4 import Dataset
 
 import numpy as np
 
 import dask.array as da
 import dask
+from dask import delayed
 import glob
 
 import pdb
@@ -115,12 +117,14 @@ class Sundask(UPlot):
     i.e. not worry about the parallel io, dask, etc
     """
 
-    def __init__(self, ncfiles, **kwargs):
+    client = None
 
+    def __init__(self, ncfiles, **kwargs):
+        self.__dict__.update(kwargs)
 
         # Load all of the files into list as xray objects
         #self._myfiles = [Sunxray(url, lazy=True) for url in filenames]
-        filenames = sorted(glob.glob(ncfiles))
+        self.filenames = sorted(glob.glob(ncfiles))
 
         def openfile(url):
             try:
@@ -135,7 +139,7 @@ class Sundask(UPlot):
             return ds
         
         self._myfiles = [openfile(url) \
-            for url in filenames]
+            for url in self.filenames]
 
         #self._myfiles=[]
         #for url in filenames:
@@ -287,7 +291,39 @@ class Sundask(UPlot):
 
         return dask.array.concatenate(arrays, axis=axis)
     
-    #####
+    #########
+    # Apply functions
+    def apply_func(self, func, varname, *args, **kwargs):
+        """
+        Apply the function to each block of data (doesn't use xarray)
+
+        See here:
+                http://dask.pydata.org/en/latest/delayed-best-practices.html
+        """
+        @delayed
+        def load_single_nc(ncfile, varname):
+            with Dataset(ncfile) as nc:
+                # Load the data
+                X = nc.variables[varname][:]
+                X[np.isnan(X)] = 0.
+            return X
+
+        @delayed
+        def lazy_func(func, X, *args, **kwargs):
+            return func(X, *args, **kwargs)
+
+        def f(func, ncfiles, varname, *args, **kwargs):
+            output = []
+            for ncfile in ncfiles:
+                X = load_single_nc(ncfile, varname)
+                output.append(lazy_func(func, X, *args, **kwargs))
+
+            return output
+
+        stack = dask.persist(f(func, self.filenames, varname, *args, **kwargs))
+        return np.concatenate([ii.compute() for ii in stack[0]], axis=-1)[...,self.ghost]
+
+    #########
     # Old
     def loadfull(self, varname, axis=0):
         """

utils/harmonic_analysis.py
@@ -30,7 +30,7 @@ def _build_lsq_A(t,frq):
         return A
     
 
-def harmonic_fit_array(t, X, frq, axis=0):
+def harmonic_fit_array(X, t, frq, axis=0):
     """
     Least-squares harmonic fit on an array
 
@@ -63,13 +63,13 @@ def harmonic_fit_array(t, X, frq, axis=0):
     A = _build_lsq_A(t,frq)
 
     # Do the least-squares fit...
-    b = np.linalg.lstsq(A,y)
+    b = np.linalg.lstsq(A,X)
    
     # reshape the array back to its original dimensions
-    b = np.reshape(b,(2*Nfrq+1,)+sz[1:])
+    output = np.reshape(b[0],(2*Nfrq+1,)+sz[1:])
     
     # Output back along the original axis
-    return b.swapaxes(axis,0)
+    return output.swapaxes(axis,0)
     
 
 def harmonic_fit(dtime, X, frq, mask=None, axis=0, phsbase=None):