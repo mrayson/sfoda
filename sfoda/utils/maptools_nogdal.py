08c00a52b7c6bc000527c8815d760222046d51f1
mrayson <matt.rayson@gmail.com>
2020-11-26 21:15:19 +0800

    Moved maptools_gdal back to maptools

---
+++ sfoda/utils/maptools_nogdal.py
@@ -0,0 +1,28 @@
+"""
+Map tools wrapper
+"""
+
+try:
+    from .maptools_gdal import *
+except:
+    print('Warning: cannot import GDAL libraries')
+
+"""
+def readShpPoly():
+    raise Exception
+
+def readShpBathy():
+    raise Exception
+
+def readraster():
+    raise Exception
+
+def readDEM():
+    raise Exception
+
+def ll2lcc():
+    raise Exception
+
+def ll2utm():
+    raise Exception
+"""
