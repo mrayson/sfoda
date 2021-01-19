7ba70f305a63201a6c51890c6c29cdd331c8cd93
mrayson <matt.rayson@gmail.com>
2021-01-19 13:10:02 +0800

    Fixed sunplotpyqt dependencies

--- sfoda/suntans/sunplotpyqt.py
+++ sfoda/suntans/sunplotpyqt.py
@@ -52,10 +52,10 @@ import sys
 
 
 
-from .sunpy import Spatial, Grid
+from sfoda.suntans.sunpy import Spatial, Grid
 from sfoda.ugrid.untrim_tools import untrim_gridvars, untrim_griddims, UNTRIMSpatial
-from sfoda.utils.ptm_tools import PtmBin
-from .suntrack import PtmNC
+#from sfoda.utils.ptm_tools import PtmBin
+#from sfoda.suntans.suntrack import PtmNC
 from datetime import datetime
 import numpy as np
 
